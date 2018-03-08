8025059133a1410ed7dba2b418e74c3ec33be8ce
Ryan Friesen <viniloruido@gmail.com>
2018-03-07 17:40:02 -0800

    Added title to buttons

--- national_park_site/main.js
+++ national_park_site/main.js
@@ -2,6 +2,8 @@ var weatherConditionsDeathValley = new XMLHttpRequest();
 var deathValleyConditionsObj;
 var weatherConditionsJoshuaTree = new XMLHttpRequest();
 var joshuaTreeConditionsObj;
+var weatherConditionsYosemite = new XMLHttpRequest();
+var yosemiteConditionsObj;
 
 weatherConditionsDeathValley.open('GET', 'http://api.wunderground.com/api/5f68e56b0fdef733/conditions/q/CA/92328.json', true);
 weatherConditionsDeathValley.responseType = 'text';
@@ -13,7 +15,7 @@ document.getElementById('death-valley-button').onclick = function() {
     
     deathValleyConditionsObj = JSON.parse(weatherConditionsDeathValley.responseText);
     console.log(deathValleyConditionsObj);
-    document.getElementById('death-valley-conditions').innerHTML = deathValleyConditionsObj.current_observation.temp_f + "°F" + deathValleyConditionsObj.current_observation.weather;
+    document.getElementById('death-valley-conditions').innerHTML = deathValleyConditionsObj.current_observation.temp_f + "°F<br>" + deathValleyConditionsObj.current_observation.weather;
   };
 
 };
@@ -28,9 +30,40 @@ document.getElementById('joshua-tree-button').onclick = function() {
     
     joshuaTreeConditionsObj = JSON.parse(weatherConditionsJoshuaTree.responseText);
     console.log(joshuaTreeConditionsObj);
-    document.getElementById('joshua-tree-conditions').innerHTML = joshuaTreeConditionsObj.current_observation.temp_f + "°F" + joshuaTreeConditionsObj.current_observation.weather;
+    document.getElementById('joshua-tree-conditions').innerHTML = joshuaTreeConditionsObj.current_observation.temp_f + "°F<br>" + joshuaTreeConditionsObj.current_observation.weather;
   };
 
 };
 
-/*95389 yosemite zip */
+weatherConditionsYosemite.open('GET', 'http://api.wunderground.com/api/5f68e56b0fdef733/conditions/q/CA/95389.json', true);
+weatherConditionsYosemite.responseType = 'text';
+weatherConditionsYosemite.send(null);
+
+document.getElementById('yosemite-button').onclick = function() {
+  
+  if(weatherConditionsYosemite.status === 200) {
+    
+    yosemiteConditionsObj = JSON.parse(weatherConditionsYosemite.responseText);
+    console.log(yosemiteConditionsObj);
+    document.getElementById('yosemite-conditions').innerHTML = yosemiteConditionsObj.current_observation.temp_f + "°F<br>" + yosemiteConditionsObj.current_observation.weather;
+  };
+
+};
+
+/*
+weatherConditionsYosemite.open('GET', 'http://api.wunderground.com/api/5f68e56b0fdef733/conditions/q/CA/95389.json', true);
+weatherConditionsYosemite.responseType = 'text';
+weatherConditionsYosemite.send(null);
+
+document.getElementById('yosemite-button').onclick = function() {
+  
+  if(weatherConditionsYosemite.status === 200) {
+    
+    yosemiteConditionsObj = JSON.parse(weatherConditionsYosemite.responseText);
+    console.log(yosemiteConditionsObj);
+    document.getElementById('yosemite-conditions').innerHTML = yosemiteConditionsObj.current_observation.temp_f + "°F<br>" + yosemiteConditionsObj.current_observation.weather;
+  };
+
+};
+
+95389 yosemite zip */
