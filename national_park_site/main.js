8a5bcd576f3744e36e28ee27dfaf1af29cf15f51
Ryan Friesen <viniloruido@gmail.com>
2018-03-07 17:28:13 -0800

    More main.js changes
    Added Joshua Tree XML request

--- national_park_site/main.js
+++ national_park_site/main.js
@@ -1,5 +1,7 @@
 var weatherConditionsDeathValley = new XMLHttpRequest();
 var deathValleyConditionsObj;
+var weatherConditionsJoshuaTree = new XMLHttpRequest();
+var joshuaTreeConditionsObj;
 
 weatherConditionsDeathValley.open('GET', 'http://api.wunderground.com/api/5f68e56b0fdef733/conditions/q/CA/92328.json', true);
 weatherConditionsDeathValley.responseType = 'text';
@@ -15,3 +17,20 @@ document.getElementById('death-valley-button').onclick = function() {
   };
 
 };
+
+weatherConditionsJoshuaTree.open('GET', 'http://api.wunderground.com/api/5f68e56b0fdef733/conditions/q/CA/92277.json', true);
+weatherConditionsJoshuaTree.responseType = 'text';
+weatherConditionsJoshuaTree.send(null);
+
+document.getElementById('joshua-tree-button').onclick = function() {
+  
+  if(weatherConditionsJoshuaTree.status === 200) {
+    
+    joshuaTreeConditionsObj = JSON.parse(weatherConditionsJoshuaTree.responseText);
+    console.log(joshuaTreeConditionsObj);
+    document.getElementById('joshua-tree-conditions').innerHTML = joshuaTreeConditionsObj.current_observation.temp_f + "°F" + joshuaTreeConditionsObj.current_observation.weather;
+  };
+
+};
+
+/*95389 yosemite zip */
